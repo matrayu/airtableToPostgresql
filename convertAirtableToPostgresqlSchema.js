5b2f5936f055150c7770f96f6a3e0848b906fbb6
Matthew Friedberg <mfriedberg@netflix.com>
2021-04-22 12:35:18 -0700

    Update convertAirtableToPostgresqlSchema.js
    Output now include PK and FK data and will import with correct linking across tables

--- convertAirtableToPostgresqlSchema.js
+++ convertAirtableToPostgresqlSchema.js
@@ -1,28 +1,39 @@
 // in browser, navigate to Airtable base API page (ie https://airtable.com/appnGoUJxA6R6XXXXX/api/docs)
 // copy and paste the script below into the devTools console
-// output with generate a postgress-like schema csv
+// output will generate a postgresql-like schema csv
 
 let baseName = application.name;
 let tableMap = [];
 let rows = [];
 
 for (const [key, value] of Object.entries(application.tablesById)) {
-    tableMap.push({id: value.id, name: value.name})
-  }
+  tableMap.push({ id: value.id, name: value.name });
+}
 
 tableMap.forEach((table, i) => {
-    let titleDataColumns = application.tablesById[`${table.id}`].columns;
+  let titleDataColumns = application.tablesById[`${table.id}`].columns;
+  let pk = application.tablesById[`${table.id}`].primaryColumnName;
 
-    titleDataColumns.forEach((column, i) => {
-        let newName = column.name.replace(/#/g, "number");
-        let newRow = [
-          `postgresql;"${baseName}";"public";"${table.name}";"${newName}";${
-            i + 1
-          };"${column.type}"`,
-        ];
-        rows.push(newRow);
-      });
-})
+  titleDataColumns.forEach((column, i) => {
+    let newRow = [];
+    let newName = column.name.replace(/#/g, "number");
+    let baseRowName = `postgresql;"${baseName}";"public";"${
+      table.name
+    }";"${newName}";${i + 1};"${column.type}";""`;
+
+    if (pk === newName) {
+      newRow = [`${baseRowName};"PRIMARY KEY";"";"";""`];
+    }
+    if (column.type === "foreignKey") {
+      newRow = [
+        `${baseRowName};"FOREIGN KEY";"public";"${column.foreignTable.name}";"${column.foreignTable.primaryColumnName}"`,
+      ];
+    } else {
+      newRow = [`${baseRowName};"";"";"";""`];
+    }
+    rows.push(newRow);
+  });
+});
 
 let csvContent =
   "data:text/csv;charset=utf-8," + rows.map((e) => e.join(",")).join("\n");
